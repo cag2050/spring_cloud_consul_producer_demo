7a6f465370705565eda1b56c42db1d90a8f985f0
chen_anguo <cag2050@163.com>
2019-09-06 11:03:14 +0800

    project init

---
+++ src/main/java/com/cag2050/spring_cloud_consul_producer_demo/SpringCloudConsulProducerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cag2050.spring_cloud_consul_producer_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConsulProducerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConsulProducerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cag2050/spring_cloud_consul_producer_demo/SpringCloudConsulProducerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cag2050.spring_cloud_consul_producer_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConsulProducerDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
