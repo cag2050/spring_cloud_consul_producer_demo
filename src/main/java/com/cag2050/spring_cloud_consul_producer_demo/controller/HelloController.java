58222d8916b5fa6c25236070f629ec001bff0e58
chen_anguo <cag2050@163.com>
2019-09-17 10:27:44 +0800

    controller add port

--- src/main/java/com/cag2050/spring_cloud_consul_producer_demo/controller/HelloController.java
+++ src/main/java/com/cag2050/spring_cloud_consul_producer_demo/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.cag2050.spring_cloud_consul_producer_demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${server.port}")
+    String port;
+
     @RequestMapping("/hello")
     String hello() {
-        return "Hello consul one";
+        return "Hello, I am from " + port;
     }
 }
